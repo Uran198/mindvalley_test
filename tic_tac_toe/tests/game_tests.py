5bff6d315bf447fef5986dc11814b61e8e57a070
Attila Tovt <uran198@gmail.com>
2016-04-07 18:08:20 +0300

    Add checking for winning condition

--- tic_tac_toe/tests/game_tests.py
+++ tic_tac_toe/tests/game_tests.py
@@ -43,7 +43,44 @@ class GameTest(unittest.TestCase):
         self.assertListEqual(self.game.state, state)
         self.assertEqual(str(ctx.exception), "Value is outside of the box.")
 
+        self.game.make_move(3, 3)
+        self.game.make_move(1, 1)
         self.game.make_move(2, 2)
         with self.assertRaises(IllegalMoveError) as ctx:
             self.game.make_move(2, 2)
         self.assertEqual(str(ctx.exception), "Place is already taken.")
+
+    def test_get_winner(self):
+        self.assertEqual(self.game.get_winner(), None)
+        player_winning = [
+            ["x..",
+             ".x.",
+             "..x"],
+            ["xxx",
+             "...",
+             "..."],
+            [".x.",
+             ".x.",
+             ".x."],
+        ]
+        ai_winning = [
+            ["..o",
+             ".o.",
+             "o.."],
+            ["o..",
+             ".o.",
+             "..o"],
+            ["ooo",
+             "...",
+             "..."],
+            [".o.",
+             ".o.",
+             ".o."],
+        ]
+        for state in player_winning:
+            self.game._state = state
+            self.assertEqual(self.game.get_winner(), "player")
+
+        for state in ai_winning:
+            self.game._state = state
+            self.assertEqual(self.game.get_winner(), "ai", state)
--- tic_tac_toe/tic/game.py
+++ tic_tac_toe/tic/game.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from .exceptions import IllegalMoveError, ImpossibleGameError
 
 
@@ -9,11 +11,12 @@ class Game:
     Assumes that player is playing with 'x' and ai - 'o'
     """
 
-    def __init__(self, lines, columns):
+    def __init__(self, lines, columns, win_count=3):
         if lines <= 0 or columns <= 0:
             raise ImpossibleGameError
         self._player_piece = "x"
         self._ai_piece = "o"
+        self._win_count = win_count
         self._state = []
         for _ in range(lines):
             line = []
@@ -22,10 +25,15 @@ class Game:
             self._state.append(line)
 
     def make_move(self, line, column):
+        """
+        Makes move on behalf of the player on the specified line and column.
+        Throws IllegalMoveError when place is already taken or the move is
+        outside of the board.
+        """
         line -= 1
         column -= 1
-        if not (0 < line < len(self._state) and
-                0 < column < len(self._state[0])):
+        if not (0 <= line < len(self._state) and
+                0 <= column < len(self._state[0])):
             raise IllegalMoveError("Value is outside of the box.")
         if self._state[line][column] != ".":
             raise IllegalMoveError("Place is already taken.")
@@ -35,3 +43,49 @@ class Game:
     @property
     def state(self):
         return [''.join(line) for line in self._state]
+
+    def get_winner(self):
+        """
+        Returns the winner based on the game state.
+        If the game state contains several winners, function may return any
+        value.
+        """
+        def winner_from_line(line):
+            if line.count(self._ai_piece) >= self._win_count:
+                return "ai"
+            if line.count(self._player_piece) >= self._win_count:
+                return "player"
+            return None
+
+        for line in self._state:
+            winner = winner_from_line(''.join(line))
+            if winner:
+                return winner
+
+        cols = defaultdict(lambda: [])
+        for line in self._state:
+            for i, col in enumerate(line):
+                cols[i] += col
+        for line in cols.values():
+            winner = winner_from_line(''.join(line))
+            if winner:
+                return winner
+
+        # Going through diagonals
+        columns = len(self._state[0])
+        for offset in range(-columns, columns):
+            main = [row[i+offset]
+                    for i, row in enumerate(self._state)
+                    if 0 <= i+offset < columns
+                    ]
+            counter = [row[-i+offset]
+                       for i, row in enumerate(self._state)
+                       if 0 <= -i+offset < columns
+                       ]
+            winner = winner_from_line(''.join(main))
+            if not winner:
+                winner = winner_from_line(''.join(counter))
+            if winner:
+                return winner
+
+        return None
