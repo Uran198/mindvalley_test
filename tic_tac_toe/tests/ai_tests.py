7c502d988e5dad0178ba5fc5ebe0da8c4c2bb54a
Attila Tovt <uran198@gmail.com>
2016-04-08 14:14:39 +0300

    Add MinimaxAI to ai module

--- tic_tac_toe/tests/ai_tests.py
+++ tic_tac_toe/tests/ai_tests.py
@@ -56,3 +56,47 @@ class SimpleAITest(unittest.TestCase):
     def test_next_move_without_legal_moves(self):
         self.game.empty_place = 't'
         self.assertRaises(NoLegalMoveError, self.ai.next_move)
+
+
+class MinimaxAITest(unittest.TestCase):
+
+    def setUp(self):
+        self.game = mock.Mock()
+        self.game.state = ['...']*3
+        self.game.empty_place = '.'
+
+        self.ai = ai.MinimaxAI(self.game, 'o')
+
+    def test_no_legal_move(self):
+        self.game.empty_place = 'l'
+        self.assertRaises(NoLegalMoveError, self.ai.next_move)
+
+    def test_score(self):
+        self.game.get_winner = mock.Mock()
+        self.game.get_winner.return_value = "ai"
+        self.assertEqual(self.ai.score("fake"), 1)
+        self.game.get_winner.return_value = "player"
+        self.assertEqual(self.ai.score("fake"), -1)
+        self.game.get_winner.return_value = None
+        self.assertEqual(self.ai.score("fake"), 0)
+
+    def test_next_move(self):
+        from tic.game import Game
+        self.game = Game(3, 3)
+        self.ai = ai.MinimaxAI(self.game, 'o')
+        self.assertIn(self.ai.next_move(),
+                      [(0, 0), (1, 1), (2, 2), (0, 2), (2, 0)])
+        self.game._state = [
+            ['o', 'x', '.'],
+            ['.', '.', '.'],
+            ['.', '.', '.']
+        ]
+        self.assertIn(self.ai.next_move(),
+                      [(1, 1), (2, 0), (1, 0)])
+        self.game._state = [
+            ['o', '.', 'x'],
+            ['.', '.', '.'],
+            ['.', '.', '.']
+        ]
+        self.assertIn(self.ai.next_move(),
+                      [(1, 1), (2, 0), (1, 0)])
--- tic_tac_toe/tic/ai.py
+++ tic_tac_toe/tic/ai.py
@@ -23,3 +23,52 @@ class SimpleAI(BasicAI):
                 if val == self._game.empty_place:
                     return (i, j)
         raise NoLegalMoveError("AI found no legal move to make.")
+
+
+class MinimaxAI(BasicAI):
+
+    def __init__(self, *args, **kwargs):
+        self._cache = {}
+        super(MinimaxAI, self).__init__(*args, **kwargs)
+
+    def next_move(self):
+        if ''.join(self._game.state).count(self._game.empty_place) == 0:
+            raise NoLegalMoveError("AI found no legal move to make.")
+        score, move = self.minimax(self._game.state, True)
+        return move
+
+    def score(self, state):
+        winner = self._game.get_winner(state)
+        if winner is None:
+            score = 0
+        elif winner == "ai":
+            score = 1
+        else:
+            score = -1
+        return score
+
+    def minimax(self, state, is_max):
+        if self._game.is_game_over(state):
+            return (self.score(state), (-1, -1))
+
+        if (''.join(state), is_max) in self._cache:
+            return self._cache[(''.join(state), is_max)]
+
+        scoremoves = []
+
+        for i, row in enumerate(state):
+            for j, val in enumerate(row):
+                if val != self._game.empty_place:
+                    continue
+                piece = self._pieces if is_max else self._game.player_piece
+                next_state = self._game.get_next_state(state, i, j, piece)
+                scoremoves.append(
+                    (self.minimax(next_state, not is_max)[0], (i, j))
+                )
+
+        if is_max:
+            result = max(scoremoves, key=lambda x: x[0])
+        else:
+            result = min(scoremoves, key=lambda x: x[0])
+        self._cache[(''.join(state), is_max)] = result
+        return result
