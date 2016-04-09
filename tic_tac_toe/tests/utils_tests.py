d2319d01398e567b4b0a029b4d26ab4ebea84d36
Attila Tovt <uran198@gmail.com>
2016-04-09 20:50:07 +0300

    Add StatesCache to cache states
    Needed for optimization based on rotations and mirroring

---
+++ tic_tac_toe/tests/utils_tests.py
@@ -0,0 +1,96 @@
+from unittest import TestCase
+
+from tic import utils
+
+
+class StatesCacheTest(TestCase):
+
+    def setUp(self):
+        self.cache = utils.StatesCache()
+
+    def test_assignment_with_equal_state(self):
+        states = [
+            ([".x.", "x..", ".oo"], True),
+            (["..o", "x.o", ".x."], True)
+        ]
+        o1, o2 = object(), object()
+        self.cache[states[0]] = o1
+        self.cache[states[1]] = o2
+        self.assertEqual(self.cache[states[0]], o2)
+        self.assertEqual(self.cache[states[1]], o2)
+
+    def test_equal_states(self):
+        states = [
+            [".x.",
+             "x..",
+             ".oo"],
+            ["..o",
+             "x.o",
+             ".x."],
+            ["oo.",
+             "..x",
+             ".x."],
+            [".x.",
+             "o.x",
+             "o.."],
+
+            [".x.",
+             "..x",
+             "oo."],
+            ["o..",
+             "o.x",
+             ".x."],
+            [".oo",
+             "x..",
+             ".x."],
+            [".x.",
+             "x.o",
+             "..o"]
+        ]
+        tmp = self.cache[(states[0], True)] = object()
+        for state in states[1:]:
+            self.assertEqual(self.cache[(state, True)], tmp, state)
+            with self.assertRaises(KeyError):
+                self.cache[(state, False)]
+
+    def test_all_equivalent_states(self):
+        state = (["ab", "cd"], True)
+        possible = {
+            ("abcd", True),
+            ("bdac", True),
+            ("dcba", True),
+            ("cadb", True),
+            ("badc", True),
+            ("dbca", True),
+            ("cdab", True),
+            ("acbd", True),
+        }
+        result = self.cache.all_equivalent_states(state)
+        self.assertSetEqual(possible, set(result))
+
+
+class RotateTest(TestCase):
+
+    def test_rotate_3x3(self):
+        state = ["ab1", "cd2", "ef3"]
+        self.assertListEqual(utils.rotate(state), ["eca", "fdb", "321"])
+
+    def test_rotate_2x3(self):
+        state = ["ab", "cd", "ef"]
+        self.assertListEqual(utils.rotate(state), ["eca", "fdb"])
+
+    def test_rotate_2x2(self):
+        state = ["ab", "cd"]
+        self.assertListEqual(utils.rotate(state), ["ca", "db"])
+
+    def test_back_rotate_3x3(self):
+        state = ["eca", "fdb", "321"]
+        self.assertListEqual(utils.back_rotate(state), ["ab1", "cd2", "ef3"])
+
+    def test_back_rotate_2x3(self):
+        state = ["eca", "fdb"]
+        self.assertListEqual(utils.back_rotate(state), ["ab", "cd", "ef"])
+
+    def test_back_rotate_2x2(self):
+        state = ["ca", "db"]
+        self.assertListEqual(utils.back_rotate(state), ["ab", "cd"])
---
+++ tic_tac_toe/tic/utils.py
@@ -0,0 +1,64 @@
+
+
+def rotate(state):
+    new_state = []
+    columns = len(state[0])
+    rows = len(state)
+    for j in range(columns):
+        line = ""
+        for i in range(rows):
+            line += state[i][j]
+        new_state.append(line[::-1])
+    return new_state
+
+
+def back_rotate(state):
+    new_state = []
+    columns = len(state[0])
+    rows = len(state)
+    for j in range(columns):
+        line = ""
+        for i in range(rows):
+            line += state[i][-j-1]
+        new_state.append(line)
+    return new_state
+
+
+class StatesCache():
+    """
+    Cache for saving states of the game.
+    """
+
+    def all_equivalent_states(self, state):
+        state, is_max = state
+        if (''.join(state), is_max) in self._eq_cache:
+            return self._eq_cache[(''.join(state), is_max)]
+        result = set()
+        for _ in range(4):
+            new_state = []
+            for line in state:
+                new_state.append(line[::-1])
+            result.add((''.join(new_state), is_max))
+            new_state = rotate(state)
+            result.add((''.join(new_state), is_max))
+            state = new_state
+        self._eq_cache[(''.join(state), is_max)] = result
+        return result
+
+    def __init__(self, *args):
+        self._eq_cache = {}
+        self._cache = {}
+
+    def __setitem__(self, key, value):
+        for state in self.all_equivalent_states(key):
+            if state in self._cache:
+                self._cache[state] = value
+                return
+        self._cache[state] = value
+
+    def __getitem__(self, key):
+        for state in self.all_equivalent_states(key):
+            if state in self._cache:
+                return self._cache[state]
+        # Let it raise error.
+        return self._cache[state]
